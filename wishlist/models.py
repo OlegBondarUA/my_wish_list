f4aca2ab8a0cf746c56c837ae37d1ff7a171ae5a
OlegBondarUA <Petrovichteh@gmail.com>
2024-08-28 12:19:41 +0300

    Create wishlist app and Wish model

---
+++ wishlist/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Wish(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.URLField(max_length=500)
+    link = models.URLField(max_length=500)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    achieved = models.BooleanField(default=False)
+    reserved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
