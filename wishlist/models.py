f2dd3fc5fe35963db26a7bd6be448f54b293fd5f
OlegBondarUA <Petrovichteh@gmail.com>
2024-08-30 13:13:19 +0300

    Add image storage configuration and upload initial wishes

--- wishlist/models.py
+++ wishlist/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Wish(models.Model):
     name = models.CharField(max_length=255)
-    image = models.URLField(max_length=500)
+    image = models.ImageField(upload_to='wishes/images/')
     link = models.URLField(max_length=500)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     achieved = models.BooleanField(default=False)
