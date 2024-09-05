f6d6ef39923cfcc15b3b6e98e56fdfa0b665e0ab
OlegBondarUA <Petrovichteh@gmail.com>
2024-09-05 16:56:59 +0300

    upd modal and migration

--- backend/wishlist/models.py
+++ backend/wishlist/models.py
@@ -1,13 +1,9 @@
 from django.db import models
 
 
-def wish_image_path(instance, filename):
-    return f'images/{filename}'
-
-
 class Wish(models.Model):
     name = models.CharField(max_length=255)
-    image = models.ImageField(upload_to=wish_image_path)
+    image = models.ImageField(upload_to='images/')
     link = models.URLField(max_length=500)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     achieved = models.BooleanField(default=False)
