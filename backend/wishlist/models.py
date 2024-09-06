e93367500d638cc89122f71d5a20f6fa2430e084
OlegBondarUA <Petrovichteh@gmail.com>
2024-09-06 23:36:02 +0300

    Add description, added_at, and reserved_at fields to Wish model

--- backend/wishlist/models.py
+++ backend/wishlist/models.py
@@ -1,13 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Wish(models.Model):
     name = models.CharField(max_length=255)
     image = models.ImageField(upload_to='images/')
+    description = models.TextField(blank=True, null=True)
     link = models.URLField(max_length=500)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     achieved = models.BooleanField(default=False)
     reserved = models.BooleanField(default=False)
+    added_at = models.DateTimeField(default=timezone.now)
+    reserved_at = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
         return self.name
