640e8ff9acc8cfa5a853188e324ef434f47d91a2
OlegBondarUA <Petrovichteh@gmail.com>
2024-09-09 14:06:53 +0300

    Add wish detail and reserve API

--- backend/wishlist/urls.py
+++ backend/wishlist/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import WishListAPIView
+from .views import WishListAPIView, WishDetailAPIView, WishReserveAPIView
 
 
 urlpatterns = [
     path('api/wishes/', WishListAPIView.as_view(), name='wish-list'),
+    path('api/wishes/<int:id>/', WishDetailAPIView.as_view(), name='wish-detail'),
+    path('api/wishes/<int:id>/reserve/', WishReserveAPIView.as_view(), name='wish-reserve'),
 ]
