6b839bbef6bd2e200db8f07fe2bd0937c94c5239
OlegBondarUA <Petrovichteh@gmail.com>
2024-09-26 13:27:13 +0300

    Add token-based login and JWT authentication logic

--- backend/wishlist/urls.py
+++ backend/wishlist/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import WishListAPIView, WishDetailAPIView, WishReserveAPIView, AddWishesAPIView
 
 
@@ -7,4 +8,7 @@ urlpatterns = [
     path('api/wishes/add/', AddWishesAPIView.as_view(), name='add-wishes'),
     path('api/wishes/<int:id>/', WishDetailAPIView.as_view(), name='wish-detail'),
     path('api/wishes/<int:id>/reserve/', WishReserveAPIView.as_view(), name='wish-reserve'),
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
